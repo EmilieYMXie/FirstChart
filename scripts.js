3bdd780531d72f5b22449e3290b955ea7bfaf94d
EmilieYMXie <ex2112@columbia.edu>
2015-02-25 19:42:43 -0500

    Formatted scripts.js annotation

--- scripts.js
+++ scripts.js
@@ -1,54 +1,57 @@
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', {
+	'packages' : ['corechart']
+});
 
-      // Load the Visualization API and the piechart package.
-      google.load('visualization', '1.0', {'packages':['corechart']});
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(drawChart);
 
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(drawChart);
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
 
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      
-      google.load('visualization','1.0',{'packages':['corechart']});
-      
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-      	
-      	var arraysData = [];
-      	//use a for loop to make an array of arrays out of relevant JSON data
-      	
-      	for(var i=0;i<jsonFREDData.observations.length; i++){
-      		
-      		var itemArray = [];
-      		itemArray.push(jsonFREDData.observations[i].date);
-      		itemArray.push(Number(jsonFREDData.observations[i].value));
-      		
-      		arraysData.push(itemArray); //push data from small list to bigger list
-      	}
+google.load('visualization', '1.0', {
+	'packages' : ['corechart']
+});
 
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows(arraysData);
+google.setOnLoadCallback(drawChart);
+function drawChart() {
 
-        // Set chart options
-        var options = {'title':'Eating Too Much Pizza',
-                       'width':400,
-                       'height':300,
-                       'hAxis':{
-                       	 },//end of hAxis
-                       	 'vAxis':{
-                       	 }//end of vAxis
-                       };//end of var options;
+	var arraysData = [];
+	//use a for loop to make an array of arrays out of relevant JSON data
 
-		options.hAxis.textStyle = myTextStyle;
-		console.log(options.hAxis.textStyle);//Making hAxis (horizontal axis) have the text style of my var myTextStyles
+	for (var i = 0; i < jsonFREDData.observations.length; i++) {
 
-		options.vAxis.textStyle = myTextStyle;
-		console.log(options.vAxis.textstyle);//Do the same for vAxis (vertical axis)
-		
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+		var itemArray = [];
+		itemArray.push(jsonFREDData.observations[i].date);
+		itemArray.push(Number(jsonFREDData.observations[i].value));
+
+		arraysData.push(itemArray);
+		//push data from small list to bigger list
+	}
+
+	// Create the data table.
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Topping');
+	data.addColumn('number', 'Slices');
+	data.addRows(arraysData);
+
+	// Set chart options
+	var options = {
+		'title' : 'Eating Too Much Pizza',
+		'width' : 400,
+		'height' : 300,
+		'hAxis' : {}, //end of hAxis
+		'vAxis' : {}//end of vAxis
+	};//end of var options;
+
+	options.hAxis.textStyle = myTextStyle;
+	console.log(options.hAxis.textStyle);//Making hAxis (horizontal axis) have the text style of my var myTextStyles
+
+	options.vAxis.textStyle = myTextStyle;
+	console.log(options.vAxis.textstyle);//Do the same for vAxis (vertical axis)
+
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+};//end of drawChart function
