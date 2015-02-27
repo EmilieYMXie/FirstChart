2328ce40c5e730c1e1c55cb61d16453320c8561d
EmilieYMXie <ex2112@columbia.edu>
2015-02-27 15:41:54 -0500

    Changed fontsize of myTextStyle

--- GoogleStylesObj.js
+++ GoogleStylesObj.js
@@ -1,18 +1,14 @@
 
 	/*
-	 * Area for my Good viz style objects: fonts, colors etc
+	 * Area for my Google viz style objects: fonts, colors etc
 	 */
 	
 var myTextStyle = {
 	fontName : 'Times-Roman',
-	fontSize : 18,
+	fontSize : 15,
 	bold : true,
 	italic : true,
 	color : '#871b47', // The color of the text.
 	auraColor : '#d799ae', // The color of the text outline.
 	opacity : 0.8 // The transparency of the text.
 };
-
-var axisFormat = {
-	format: 'MMM d, y'
-}
