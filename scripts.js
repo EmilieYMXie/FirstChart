2810addbbfd8b7a8f9983f6f7c2da4042536d64b
EmilieYMXie <ex2112@columbia.edu>
2015-03-11 20:39:56 -0400

    Adjust date and add annotations column

--- scripts.js
+++ scripts.js
@@ -23,7 +23,8 @@ function drawChart() {
 	for (var i = 0; i < jsonFREDData.observations.length; i++) {
 
 		var itemArray = [];
-		itemArray.push(jsonFREDData.observations[i].date);
+		itemArray.push(new Date(jsonFREDData.observations[i].date));
+		//added (new Date()) when dealing with dates, have to add new Date, with numbers it's just Numbers'
 		itemArray.push(Number(jsonFREDData.observations[i].value));
 
 		arraysData.push(itemArray);
@@ -32,38 +33,51 @@ function drawChart() {
 
 	// Create the data table.
 	var data = new google.visualization.DataTable();
-	data.addColumn('string', 'Topping');
+	data.addColumn('date', 'Date');
+	//date is it's own data type'
 	data.addColumn('number', 'Slices');
+	data.addColumn({type:'string', role:'annotation'}); // annotation role col.
 	data.addRows(arraysData);
 
+	var formatter = new google.visualization.DateFormat({
+		pattern : 'MMM d,y'
+	});
+	//reformat our data
+	formatter.format(data, 0);
+
+	var hAxisLabelFormat = 'MMM d, y';
 	// Set chart options
 	var options = {
 		'title' : 'Eating Too Much Pizza',
-		'width' : 600,
+		'width' : 700,
 		'height' : 500,
 		'hAxis' : {
-			'title': 'Horizontal Axis Title',
-			'gridlines':{},
-			'format':{}
+			'title' : 'Horizontal Axis Title',
+			'gridlines' : {},
 		}, //end of hAxis
 		'vAxis' : {}//end of vAxis
-	
-	};//end of var options;
 
-	options.hAxis.textStyle = myTextStyle;
-	console.log(options.hAxis.textStyle);//Making hAxis (horizontal axis) have the text style of my var myTextStyles
+	};
+	//end of var options;
+//end of var hAxisLabelFormat
 
-	options.vAxis.textStyle = myTextStyle;
-	console.log(options.vAxis.textstyle);//Do the same for vAxis (vertical axis)
-	
-	options.hAxis.gridlines = axisGridlines;
-	console.log(options.hAxis.gridlines);
-	
-	options.vAxis.gridlines = axisGridlines;
-	console.log(options.vAxis.gridlines);
-		
 
-	// Instantiate and draw our chart, passing in some options.
-	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-	chart.draw(data, options);
+options.hAxis.format = hAxisLabelFormat;
+options.hAxis.textStyle = myTextStyle;
+console.log(options.hAxis.textStyle);
+//Making hAxis (horizontal axis) have the text style of my var myTextStyles
+
+options.vAxis.textStyle = myTextStyle;
+console.log(options.vAxis.textstyle);
+//Do the same for vAxis (vertical axis)
+
+options.hAxis.gridlines = axisGridlines;
+console.log(options.hAxis.gridlines);
+
+options.vAxis.gridlines = axisGridlines;
+console.log(options.vAxis.gridlines);
+
+// Instantiate and draw our chart, passing in some options.
+var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+chart.draw(data, options);
 };//end of drawChart function
